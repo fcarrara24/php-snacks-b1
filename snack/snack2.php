cc2451e105808cc81bbeb1c79709b77acf3f8128
unknown <f.carrara24@studenti.unibg.it>
2023-11-28 16:13:23 +0100

    snack 2

--- snack/snack2.php
+++ snack/snack2.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +11,56 @@
 </head>
 <body>
     <a href="../index.php">index</a>
+
+    <?php
+    $nome= $_GET['nome'];
+    $email= $_GET['email'];
+    $age = $_GET['age'];
+    
+    if(!empty($nome)&& !empty($email) && !empty($age)){
+        if(is_numeric($age) &&strlen($nome)>3 && str_contains($email, '@')){
+            echo '<div>Accesso riuscito</div>';
+        } else {
+            echo '<div>Accesso negato</div>';  
+            if (!is_numeric($age)){
+                echo 'l\'eta deve essere un numero';
+            }
+            if (!strlen($nome)>3){
+                echo 'il nome deve essere piu lungo di 3 caratteri';
+            }
+            if (!str_contains( $email, '@')){
+                echo 'la mail deve contenere una @';
+            }
+        }
+        
+    }
+    else {?>
+         <form method="get">
+                <div class="form-group">
+                    <label for="nome">Name</label>
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="nome">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+            
+                <div class="form-group">
+                    <label class="form-check-label" for="age" >eta</label>
+                    <input type="number" class="form-control" id="age" name="age" placeholder="eta">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    <?php
+    }
+
+
+    ?>
+    
+
 </body>
 </html>
+
+
